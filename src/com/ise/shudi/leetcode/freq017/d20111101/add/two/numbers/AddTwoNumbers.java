86c834c8ef0afb940be385a525c8b781239561ac
Shudi Wang <shudiwsh2009@gmail.com>
2014-12-10 14:43:38 +0800

    rename package

---
+++ src/com/ise/shudi/leetcode/freq017/d20111101/add/two/numbers/AddTwoNumbers.java
@@ -0,0 +1,71 @@
+package com.ise.shudi.leetcode.freq017.d20111101.add.two.numbers;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode l1 = new ListNode(2);
+		l1.next = new ListNode(4);
+		l1.next.next = new ListNode(3);
+		ListNode l2 = new ListNode(5);
+		l2.next = new ListNode(6);
+		l2.next.next = new ListNode(4);
+		AddTwoNumbers atn = new AddTwoNumbers();
+		ListNode r = atn.addTwoNumbers(l1, l2);
+		while(r.next != null) {
+			System.out.print(r.val + "->");
+			r = r.next;
+		}
+		System.out.print(r.val);
+	}
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode result = new ListNode(0);
+		ListNode cur = result;
+		int flag = 0;
+		while(l1 != null && l2 != null) {
+			int s = l1.val + l2.val + flag;
+			flag = (s >= 10) ? 1 : 0;
+			s = (s >= 10) ? (s - 10) : s;
+			ListNode node = new ListNode(s);
+			cur.next = node;
+			cur = node;
+			l1 = l1.next;
+			l2 = l2.next;
+		}
+		while(l1 != null) {
+			int s = l1.val + flag;
+			flag = (s >= 10) ? 1 : 0;
+			s = (s >= 10) ? (s - 10) : s;
+			ListNode node = new ListNode(s);
+			cur.next = node;
+			cur = node;
+			l1 = l1.next;
+		}
+		while(l2 != null) {
+			int s = l2.val + flag;
+			flag = (s >= 10) ? 1 : 0;
+			s = (s >= 10) ? (s - 10) : s;
+			ListNode node = new ListNode(s);
+			cur.next = node;
+			cur = node;
+			l2 = l2.next;
+		}
+		if(flag != 0) {
+			ListNode node = new ListNode(flag);
+			cur.next = node;
+		}
+		return result.next;
+	}
+
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
---
+++ src/com/ise/shudi/leetcode/freq072/d20110515/longest/substring/without/repeating/characters/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,32 @@
+package com.ise.shudi.leetcode.freq072.d20110515.longest.substring.without.repeating.characters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "abcabcab";
+		LongestSubstringWithoutRepeatingCharacters lswrc = new LongestSubstringWithoutRepeatingCharacters();
+		System.out.println(lswrc.lengthOfLongestSubstring(s));
+	}
+	
+	public int lengthOfLongestSubstring(String s) {
+		char[] cs = s.toCharArray();
+		Map<Character, Integer> hmCharIndex = new HashMap<Character, Integer>();
+		int lastIndex = -1;
+		int max = 0;
+		for(int i = 0; i < cs.length; ++i) {
+			if(hmCharIndex.containsKey(cs[i]) && lastIndex < hmCharIndex.get(cs[i])) {
+				lastIndex = hmCharIndex.get(cs[i]);
+				hmCharIndex.put(cs[i], i);
+			} else {
+				max = Math.max(max, i - lastIndex);
+				hmCharIndex.put(cs[i], i);
+			}
+		}
+		return max;
+    }
+
+}
