e60da8ca8258f26d21a29a259481a398dcb2758c
Shudi Wang <shudiwsh2009@gmail.com>
2014-12-12 17:06:59 +0800

    018-020

---
+++ src/com/ise/shudi/leetcode/freq020/d20120212/generate/parentheses/GenerateParentheses.java
@@ -0,0 +1,38 @@
+package com.ise.shudi.leetcode.freq020.d20120212.generate.parentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public List<String> generateParenthesis(int n) {
+		List<String> result = new ArrayList<String>();
+		StringBuilder sb = new StringBuilder();
+		generateRecursively(n, result, sb, 0, 0);
+		return result;
+	}
+	
+	private void generateRecursively(int n, List<String> result, StringBuilder sb,
+			int used, int left) {
+		if(used == n && left == 0) {
+			result.add(sb.toString());
+			return;
+		}
+		if(used < n) {
+			sb.append("(");
+			generateRecursively(n, result, sb, used + 1, left + 1);
+			sb.deleteCharAt(sb.length() - 1);
+		}
+		if(left > 0) {
+			sb.append(")");
+			generateRecursively(n, result, sb, used, left - 1);
+			sb.deleteCharAt(sb.length() - 1);
+		}
+	}
+
+}
